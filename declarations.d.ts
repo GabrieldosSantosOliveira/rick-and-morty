18c45d3742d61e6e10102d77d08e70f63e757c99
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-07-05 15:57:13 -0300

    chore: setup jest

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="@testing-library/jest-native" />
+
 declare module '*.jpg' {
   const value: import('react-native').ImageSourcePropType;
   export default value;
---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { compilerOptions } = require('./tsconfig.json')
+const {pathsToModuleNameMapper} = require('ts-jest')
+/** @type {import('jest').Config} */
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  setupFiles: [
+    '<rootDir>/jest/jest-setup.ts',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  preset: 'jest-expo',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+}
